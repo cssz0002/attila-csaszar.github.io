435b0a8a7aa5733984a2c96f159ec3c0b5b95331
cssz0002 <cssz0002@hz.nl>
2022-09-18 21:02:34 +0200

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+var progressBar = document.querySelector('.progress_bar')
+var progressFillout = document.querySelector('.progress_bar_fillout')
+
+console.log(progressBar.getClientRects())
+
+console.log(progressFillout.getBoundingClientRect().width)
+console.log(progressBar.getBoundingClientRect().width)
