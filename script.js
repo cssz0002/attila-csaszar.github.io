f2083fab96e83039944c5ac2ef694041c235a8ce
cssz0002 <cssz0002@hz.nl>
2022-09-23 11:45:15 +0200

    progress bar first steps

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-var progressBar = document.querySelector('.progress_bar')
-var progressFillout = document.querySelector('.progress_bar_fillout')
+let grade_cells = document.getElementsByClassName('grade')
+console.log(grade_cells)
+let osszeg = 0
 
-console.log(progressBar.getClientRects())
+for (let i = 0; i < 4; ++i) {
+  if (grade_cells[i].innerHTML != '') {
+    if (parseFloat(grade_cells[i].innerHTML >= 5.5)) {
+      osszeg +=
+        grade_cells[i].previousElementSibling.previousElementSibling.innerHTML
+    }
+  }
+}
 
-console.log(progressFillout.getBoundingClientRect().width)
-console.log(progressBar.getBoundingClientRect().width)
+function megnez(hanyat, cells) {
+    let ec = (cells.previousElementSibling.previousElementSibling.innerHTML);
+    for (let i = 0; i < hanyat; ++i) {
+    if (cells.innerHTML == '' || ) {
+      osszeg += if(parseFloat(cells[i].innerHTML <= 5.5) {
+        return 0;
+      }
+    }
+  }
+}
+
+for (let i = 0; i < grade_cells.length; ++i) {
+  if (i < 4) {
+    osszeg += megnez(1, [grade_cells[i]])
+    if (i == 5) {
+      osszeg += megnez()
+    }
+  }
+}
+
+document.getElementsByClassName('progress_bar_fillout')[0],
+  (style.width = (osszeg / 60) * 100 + '%')
